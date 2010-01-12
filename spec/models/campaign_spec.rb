557fc886e72ff41be5bd8e962ab4ca4c8f18dc56
cho <cho@06daa6dd-5c14-464e-8a85-0d68c524be32>
2010-01-12 02:55:37 +0000

    テストコード調整
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/elecoma/trunk@22 06daa6dd-5c14-464e-8a85-0d68c524be32

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Campaign do
-  fixtures :campaigns,:campaigns_customers,:customers
+  fixtures :campaigns,:campaigns_customers,:customers,:products,:product_styles
   
   before(:each) do
     #商品コードなし
