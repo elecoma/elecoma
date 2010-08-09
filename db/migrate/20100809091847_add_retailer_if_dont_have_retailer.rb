1820da67ef883abca0af2b0e10af78a549f8bf2c
Taro Matsuzawa <tmatsuzawa@kbmj.com>
2010-08-09 18:37:49 +0900

    シーケンスがおかしくなるのを修正

--- db/migrate/20100809091847_add_retailer_if_dont_have_retailer.rb
+++ db/migrate/20100809091847_add_retailer_if_dont_have_retailer.rb
@@ -4,9 +4,12 @@ class AddRetailerIfDontHaveRetailer < ActiveRecord::Migration
       r = Retailer.find(1)
     rescue ActiveRecord::RecordNotFound
       r = Retailer.new
-      r.id = 1
       r.name = "メイン販売元"
       r.save!
+      if r.id != 1
+        r.id = 1
+        r.save!
+      end
     rescue
       raise ActiveRecord::RecordNotFound
     end
