05e1ce66809ec58bffc1043a681b9a17418b949f
Taro Matsuzawa <tmatsuzawa@kbmj.com>
2010-08-20 16:19:35 +0900

    決済プラグインのベースとなるmigrateを追加

---
+++ db/migrate/20100819115306_add_payment_plugin_id_to_payment.rb
@@ -0,0 +1,9 @@
+class AddPaymentPluginIdToPayment < ActiveRecord::Migration
+  def self.up
+    add_column :payments, :payment_plugin_id, :integer, :comment => "支払いプラグインID"
+  end
+
+  def self.down
+    remove_column :payments, :payment_plugin_id
+  end
+end
