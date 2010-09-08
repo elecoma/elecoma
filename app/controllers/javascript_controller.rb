78b392d92ceb056c9c7be1f5763b1fbe3603b283
Taro Matsuzawa <tmatsuzawa@kbmj.com>
2010-09-08 18:35:39 +0900

    javascript controllerでSSLが無効になってしまう問題に対応

--- app/controllers/javascript_controller.rb
+++ app/controllers/javascript_controller.rb
@@ -1,5 +1,6 @@
 class JavascriptController < ApplicationController
   caches_page :application
+  ssl_allowed :application, :thickbox, :treemenu
   after_filter {|c| c.headers["Content-Type"] = 'application/javascript' }
   def application
   end
