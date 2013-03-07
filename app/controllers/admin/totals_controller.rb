c3760e23cd5ec61a5325bb9eb75d715a601d1a15
ymatsumoto <ymatsumoto@appirits.com>
2013-03-07 18:33:59 +0900

    ピッキングリストと納品書を単体で表示するのをやめ、 PDF出力できるようにした

--- app/controllers/admin/totals_controller.rb
+++ app/controllers/admin/totals_controller.rb
@@ -5,41 +5,51 @@ class Admin::TotalsController < Admin::BaseController
   before_filter :admin_permission_check_term
 
   def index
-
+    
+    #searchの日付をparse
     params[:search] ||= {}
     [:month, :date_from, :date_to, :sale_start_from, :sale_start_to].each do | key |
       params[:search][key] = parse_date_select(params[:search], key)
     end
+    
     params[:search][:retailer_id] ||= session[:admin_user].retailer_id
+    
     if !session[:admin_user].master_shop? && params[:search][:retailer_id] != session[:admin_user].retailer_id
       raise ActiveRecord::RecordNotFound
     end
+    
     @search = OpenStruct.new(params[:search])
     params[:page] ||= 'term'
     @agent = Totalizer.get_instance(params[:page])
+    
     if not @agent
       params[:page] = 'term'
       @agent = Totalizer.get_instance(params[:page])
     end
+    
     @sale_start_enabled = (params[:page] == 'product')
     params[:type] ||= @agent.default_type
     @title = @agent.title
     @list_view = @agent.columns
     @links = @agent.links
     @labels = @agent.labels
+    
     begin
       @records = @agent.get_records(params)
     rescue => e
       logger.error e.message
       e.backtrace.each{|bt|logger.error(bt)}
     end
+    
     @total = @agent.total
+    
     begin
       flash[:graph] = @agent.graph
     rescue =>e
       logger.error(e.message)
       e.backtrace.each{|bt|logger.error(bt)}
     end
+    
     @selected_retailer = params[:search][:retailer_id].to_i
   end
 
