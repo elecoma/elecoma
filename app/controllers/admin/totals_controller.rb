08b378f95858863ef92fe5692651ca33a08d6c80
ymatsumoto <ymatsumoto@appirits.com>
2013-03-18 19:14:43 +0900

    商品別集計が正しく集計されない不具合修正

--- app/controllers/admin/totals_controller.rb
+++ app/controllers/admin/totals_controller.rb
@@ -6,43 +6,55 @@ class Admin::TotalsController < Admin::BaseController
 
   def index
     
-    #searchの日付をparse
+    #searchに商品集計用の要素を追加
     params[:search] ||= {}
     [:month, :date_from, :date_to, :sale_start_from, :sale_start_to].each do | key |
       params[:search][key] = parse_date_select(params[:search], key)
     end
-    
+   
+    #販売元id=ログインしているユーザーの販売元id 
     params[:search][:retailer_id] ||= session[:admin_user].retailer_id
-    
+   
+    #ログインユーザーのショップが無くて販売元idが違ったらエラー 
     if !session[:admin_user].master_shop? && params[:search][:retailer_id] != session[:admin_user].retailer_id
       raise ActiveRecord::RecordNotFound
     end
     
+    #OpenStructのsearch生成
     @search = OpenStruct.new(params[:search])
+
+    #:pageがnilだったら代入
     params[:page] ||= 'term'
+
+    #:pageのクラス取得
     @agent = Totalizer.get_instance(params[:page])
     
+    #取得できなかったら取得
     if not @agent
       params[:page] = 'term'
       @agent = Totalizer.get_instance(params[:page])
     end
     
+    #productのページだったらtrue
     @sale_start_enabled = (params[:page] == 'product')
     params[:type] ||= @agent.default_type
+    
+    #
     @title = @agent.title
     @list_view = @agent.columns
     @links = @agent.links
     @labels = @agent.labels
+    @total = @agent.total
     
+    #当てはまったものをレコードに入れる
     begin
       @records = @agent.get_records(params)
     rescue => e
       logger.error e.message
       e.backtrace.each{|bt|logger.error(bt)}
     end
-    
-    @total = @agent.total
-    
+
+    #
     begin
       flash[:graph] = @agent.graph
     rescue =>e
@@ -50,6 +62,7 @@ class Admin::TotalsController < Admin::BaseController
       e.backtrace.each{|bt|logger.error(bt)}
     end
     
+    #
     @selected_retailer = params[:search][:retailer_id].to_i
   end
 
