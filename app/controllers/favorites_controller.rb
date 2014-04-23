ec37a0cb0c9e99680f0d86220eb0f7a6429354f8
nakanishi <nakanishi@appirits.com>
2014-04-24 02:30:45 +0900

    お気に入り機能：追加および削除時の不具合修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,11 +4,21 @@ class FavoritesController < BaseController
   def delete_favorite
     if @login_customer
       if params[:product_style_ids].is_a? Array
-        begin
-          Favorite.delete_all(:customer_id => @login_customer.id, :product_style_id => params[:product_style_ids].map(&:to_i))
-          flash[:notice] = "削除しました"
-        rescue
-          flash[:error] = "削除に失敗しました"
+        params[:product_style_ids].each do |product_style_id|
+          if ProductStyle.find_by_id(product_style_id).blank?
+            flash[:error] = "商品ID[" + product_style_id + "]は存在しない商品IDのため削除できませんでした"
+          else
+            if Favorite.find_by_customer_id_and_product_style_id(@login_customer,product_style_id).blank?
+              flash[:error] = "商品ID[" + product_style_id + "]はお気に入りに登録されていません"
+            else
+              begin
+                Favorite.delete_all(:customer_id => @login_customer.id, :product_style_id => product_style_id)
+                flash[:notice] = "削除しました"
+              rescue
+                flash[:error] = "削除に失敗しました"
+              end
+            end 
+          end
         end
       end
     end
@@ -17,16 +27,20 @@ class FavoritesController < BaseController
 
   def add_favorite
     if @login_customer
-      if params[:product_style_id].blank?
-        flash[:error] = "お気に入りへの追加に失敗しました"
-        return redirect_to(:controller => 'accounts', :action => 'favorite')
-      end
+      if ProductStyle.find_by_id(params[:product_style_id]).present?
+        if params[:product_style_id].blank?
+          flash[:error] = "お気に入りへの追加に失敗しました"
+          return redirect_to(:controller => 'accounts', :action => 'favorite')
+        end
 
-      favorite = Favorite.create(customer_id: @login_customer.id, product_style_id: params[:product_style_id].to_i)
-      if favorite.errors.present?
-        flash[:error] = favorite.errors.on(:product_style_id)
+        favorite = Favorite.create(customer_id: @login_customer.id, product_style_id: params[:product_style_id].to_i)
+        if favorite.errors.present?
+          flash[:error] = favorite.errors.on(:product_style_id)
+        else
+          flash[:notice] = "商品をお気に入りに登録しました"
+        end
       else
-        flash[:notice] = "商品をお気に入りに登録しました"
+        flash[:error] = "存在しない商品IDのため登録できません"
       end
     end
     redirect_to(:controller => 'accounts', :action => 'favorite')
